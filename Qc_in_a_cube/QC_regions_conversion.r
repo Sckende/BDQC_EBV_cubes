b7ffd04d351ec88d068be931aadf57380ecd5940
Claire <clrccljuhasz@gmail.com>
2023-09-06 13:48:28 -0400

    :tada: conversion des fichiers au niveau régional et sous-régional

--- Qc_in_a_cube/QC_regions_conversion.r
+++ Qc_in_a_cube/QC_regions_conversion.r
@@ -15,3 +15,13 @@ sreg_sf <- sf::st_as_sf(sreg)
 x11()
 plot(st_geometry(sreg_sf), border = "grey")
 plot(st_geometry(reg_sf), add = T)
+
+# sf::st_write(reg_sf, "/home/claire/BDQC-GEOBON/data/QUEBEC_regions/QUEBEC_regions_nat.gpkg")
+sf::st_write(sreg_sf, "/home/claire/BDQC-GEOBON/data/QUEBEC_regions/QUEBEC_SOUSregions_nat.gpkg")
+
+rm(list = ls())
+reg <- sf::st_read("/home/claire/BDQC-GEOBON/data/QUEBEC_regions/QUEBEC_regions_nat.gpkg")
+plot(st_geometry(reg))
+
+sousReg <- sf::st_read("/home/claire/BDQC-GEOBON/data/QUEBEC_regions/QUEBEC_SOUSregions_nat.gpkg")
+plot(st_geometry(sousReg))
