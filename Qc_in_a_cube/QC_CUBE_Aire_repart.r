0c8e9f3fae715b1a8086ebc4eb9b28a8aafea5e2
Claire <clrccljuhasz@gmail.com>
2023-09-11 11:24:07 -0400

    save the first layer of the cube

--- Qc_in_a_cube/QC_CUBE_Aire_repart.r
+++ Qc_in_a_cube/QC_CUBE_Aire_repart.r
@@ -10,32 +10,39 @@ list_spe_short <- list.files("/home/claire/BDQC-GEOBON/data/Bellavance_data/terr
 years <- 1992:2017
 final_areas <- NULL
 for (i in seq_len(length(list_spe_long))) {
-
     print(list_spe_short[i])
 
-    if(length(list.files(list_spe_long[i])) != 0) {
-        raster <- terra::rast(paste(list_spe[i], "maps_range.tif", sep = "/"))
+    if (length(list.files(list_spe_long[i])) != 0) {
+        raster <- terra::rast(paste(list_spe_long[i], "maps_range.tif", sep = "/"))
         species <- list_spe_short[i]
 
         areas <- lapply(raster, function(x) {
-            sum(values(x), na.rm = T)*10*10 # en km2
+            sum(values(x), na.rm = T) * 10 * 10 # en km2
         })
     } else {
         areas <- rep(NA, length(years))
     }
 
-    final_areas <- rbind(final_areas, areas)
+    final_areas <- rbind(final_areas, as.numeric(areas))
 }
 
 class(final_areas)
+head(final_areas)
+
 df <- as.data.frame(final_areas)
 df <- cbind(list_spe_short, df)
-names(df) <- c("species", years)
+
+summary(df)
+dim(df)
+
+names(df) <- c("species", as.character(years))
+
+
 head(df)
 class(df)
-summary(df$2008)
+summary(df$"2008")
 
 matplot(t(df), type = "l")
 
 ### ===> register the result !
-write.table(df, "/home/claire/BDQC-GEOBON/data/QC_in_a_cube/Species_distribution/QC_spe_distr_first_layer.txt")
+# write.table(df, "/home/claire/BDQC-GEOBON/data/QC_in_a_cube/Species_distribution/QC_spe_distr_first_layer.txt", sep = "\t")
