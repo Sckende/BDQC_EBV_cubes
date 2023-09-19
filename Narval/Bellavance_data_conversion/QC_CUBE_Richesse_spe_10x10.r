1c59a5cdbd81c0bf720f99e90be3df06e1089bff
Claire <clrccljuhasz@gmail.com>
2023-09-19 10:54:50 -0400

    :recycle: Parallelisation du calcul de richesse spe 10x10

---
+++ Narval/Bellavance_data_conversion/QC_CUBE_Richesse_spe_10x10.r
@@ -0,0 +1,47 @@
+library(sf)
+
+# Creation of an empty LIST
+rs_10x10_year <- list()
+# saveRDS(rs_10x10_year, "/home/claire/BDQC-GEOBON/data/QC_in_a_cube/Richesse_spe/QC_CUBE_Richesse_spe_10x10_LIST.rds")
+
+
+# Get array number
+i <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+
+# Get the polygon
+pol <- st_read("./data/QUEBEC_GRID_10x10.gpkg",
+    query = paste("SELECT * FROM QUEBEC_GRID_10x10 WHERE ID=", i, sep = "")
+)
+
+# spatial filter
+pix_number <- i
+wkt <- st_as_text(st_geometry(pol))
+
+# Species richness computation
+rich_spe <- NULL
+
+for (j in 1990:2019) {
+    year <- j
+
+    occ <- st_read("./data/total_occ_pres_only_versionR_UTM.gpkg",
+        query = paste("SELECT * FROM total_occ_pres_only_versionR WHERE year_obs=", year, sep = ""),
+        wkt_filter = wkt,
+        quiet = T
+    )
+
+    rich <- length(unique(occ$species))
+
+    print(paste("------------------------> Richesse spe dans polygone ", i, " est de ", rich, " pour l'année ", year, ".", sep = ""))
+
+    rich_spe <- c(rich_spe, rich)
+}
+
+row_df <- c(reg, rich_spe)
+
+ll <- readRDS("./data/QC_CUBE_Richesse_spe_10x10_LIST.rds")
+ll[[i]] <- row_df
+
+saveRDS(
+    ll,
+    "./data/QC_CUBE_Richesse_spe_10x10_LIST.rds"
+)
---
+++ Narval/Bellavance_data_conversion/sbatch_QC_CUBE_Richesse_spe_10x10.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --array=1-3
+#SBATCH --account=def-dgravel
+#SBATCH -t 00:20:00
+#SBATCH --mem=10000
+#SBATCH --job-name=QC_CUBE_richesse_sp_10x10
+#SBATCH --mail-user=juhc3201@usherbrooke.ca
+#SBATCH --mail-type=ALL
+
+echo $SLURM_ARRAY_TASK_ID
+
+module load StdEnv/2020 gcc/9.3.0 gdal/3.5.1 r/4.3.1
+Rscript QC_CUBE_Richesse_spe_10x10.r.r
